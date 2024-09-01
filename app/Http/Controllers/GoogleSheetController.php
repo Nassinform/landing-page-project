cf3a4a8cec0afe7ec2a6eebba5cb0b45ea58f7f6
yacine messouaf <nassimmerri123@gmail.com>
2024-09-01 15:23:18 +0100

    01 09 24

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Revolution\Google\Sheets\Facades\Sheets;
+use Google\Client;
+use Google\Service\Sheets;
 
 class GoogleSheetController extends Controller
 {
+    private function getGoogleClient($projectConfig)
+    {
+        $client = new Client();
+        $client->setApplicationName($projectConfig['application_name']);
+        $client->setScopes($projectConfig['scopes']);
+        $client->setAuthConfig($projectConfig['service']['file']);
+        $client->setAccessType($projectConfig['access_type']);
+
+        return $client;
+    }
+
     public function index()
     {
         try {
-            // Configurer les services pour le premier projet
-            //config(['services.google_sheets' => config('google.project_1')]);
+            $project1Config = config('google.project_1');
+            $client = $this->getGoogleClient($project1Config);
 
-            $sheets = Sheets::spreadsheet('1UZUwIgGrX0Zgz-61iEc4fWcFtwOTfrS1yjft1HrnCwA')
-                ->sheet("Youcan-Orders")
-                ->get();
+            $service = new Sheets($client);
+            $spreadsheetId = '1UZUwIgGrX0Zgz-61iEc4fWcFtwOTfrS1yjft1HrnCwA';
+            $range = 'Youcan-Orders';
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
 
-            $header = $sheets->pull(0);
-            $values = Sheets::collection($header, $sheets);
-            $valuesArray = $values->toArray();
-
-            return view('sheets.index', compact('header', 'valuesArray'));
+            dd($values);
         } catch (\Exception $e) {
             dd('Error in Project 1:', $e->getMessage());
         }
@@ -29,18 +39,16 @@ class GoogleSheetController extends Controller
     public function index2()
     {
         try {
-            // Configurer les services pour le deuxième projet
-            config(['services.google_sheets' => config('google.project_2')]);
+            $project2Config = config('google.project_2');
+            $client = $this->getGoogleClient($project2Config);
 
-            $sheets = Sheets::spreadsheet('18Rn15rkcxfKbCWHFveVr5RxU9kXS_aBawAvImfBVE9s')
-                ->sheet("Youcan-Orders")
-                ->get();
+            $service = new Sheets($client);
+            $spreadsheetId = '18Rn15rkcxfKbCWHFveVr5RxU9kXS_aBawAvImfBVE9s';
+            $range = 'Youcan-Orders';
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
 
-            $header = $sheets->pull(0);
-            $values = Sheets::collection($header, $sheets);
-            $valuesArray = $values->toArray();
-
-            return view('sheets.index1', compact('header', 'valuesArray'));
+            dd($values);
         } catch (\Exception $e) {
             dd('Error in Project 2:', $e->getMessage());
         }
